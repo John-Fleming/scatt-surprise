2661d8658d5274ac79d8ae1b671c862019a8011e
John Fleming <johndeclanfleming@gmail.com>
2020-06-07 21:22:00 -0500

    navbar routing works

--- src/components/shared/MyNavbar/MyNavbar.js
+++ src/components/shared/MyNavbar/MyNavbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink as RRNavLink } from 'react-router-dom';
 import {
   Collapse,
   Navbar,
@@ -41,13 +42,13 @@ class MyNavbar extends React.Component {
         return (
         <Nav className="ml-auto" navbar>
           <NavItem>
-            <NavLink href="/components/">Components</NavLink>
+            <NavLink tag={RRNavLink} to='/home'>Home</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/components/">Components</NavLink>
+            <NavLink tag={RRNavLink} to='/new'>New Scat</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/components/">Components</NavLink>
+            <NavLink onClick={this.logMeOut} href="#">Logout</NavLink>
           </NavItem>
         </Nav>
         );
@@ -57,7 +58,6 @@ class MyNavbar extends React.Component {
 
     return (
       <div className="myNavbar">
-        {/* <button className="btn btn-outline-dark" onClick={this.logMeOut}>Logout</button> */}
         <Navbar color="info" light expand="md">
           <NavbarBrand href="/">Scat Surprise</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
