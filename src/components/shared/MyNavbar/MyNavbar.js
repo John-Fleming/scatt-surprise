4e006796ba515ac2a14ad13ae519c331178d2e0a
John Fleming <johndeclanfleming@gmail.com>
2020-06-07 20:53:41 -0500

    installed reacstrap and added basic navbar

--- src/components/shared/MyNavbar/MyNavbar.js
+++ src/components/shared/MyNavbar/MyNavbar.js
@@ -1,33 +1,74 @@
 import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
+
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import './MyNavbar.scss';
 
 class MyNavbar extends React.Component {
+  state = {
+    isOpen: false,
+  }
+
   logMeOut = (e) => {
     e.preventDefault();
     firebase.auth().signOut();
   }
 
+  toggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
   render() {
+    const { isOpen } = this.state;
     return (
       <div className="myNavbar">
-        <nav className="navbar navbar-expand-lg bg-info">
-          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a className="navbar-brand" href="#">Scat Surprise</a>
-          {/* eslint-disable-next-line max-len */}
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item active">
-                <button className="btn btn-outline-dark" onClick={this.logMeOut}>Logout</button>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        {/* <button className="btn btn-outline-dark" onClick={this.logMeOut}>Logout</button> */}
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Scat Surprise</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+            <NavbarText>Simple Text</NavbarText>
+          </Collapse>
+        </Navbar>
       </div>
     );
   }
