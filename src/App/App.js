e5fdc67c42a4eaa54b3054a422d98dcbfa04a143
John Fleming <johndeclanfleming@gmail.com>
2020-06-07 23:18:48 -0500

    added card style

--- src/App/App.js
+++ src/App/App.js
@@ -63,7 +63,7 @@ class App extends React.Component {
         <BrowserRouter>
           <React.Fragment>
             <MyNavbar authed={authed} />
-            <div className="container">
+            <div id="app-main" className="container">
               <div className="row">
                 <Switch>
                   <PrivateRoute path='/home' component={Home} authed={authed}/>
